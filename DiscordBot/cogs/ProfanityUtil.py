a7ee1e692877c74863a3ea31c0d9deffdabcc031
Cheerfulmoss <41566305+Cheerfulmoss@users.noreply.github.com>
2021-09-29 15:50:13 +1000

    Update ProfanityUtil.py

--- DiscordBot/cogs/ProfanityUtil.py
+++ DiscordBot/cogs/ProfanityUtil.py
@@ -52,16 +52,15 @@ def surface_search(message: str, guild_id, word_list):
         word_count = properties[str(guild_id)]["wordcount"]
     word_count += 1
 
-    words = str(message).split()
 
     for word in word_list["swearwords"]:
-        if word in words:
+        if word in message:
             if server_settings[f"{guild_id}"]["swearwords"].lower() == "true":
                 swear_word = True
             swear_count += 1
 
     for word in word_list["slurs"]:
-        if word in words:
+        if word in message:
             if server_settings[f"{guild_id}"]["slurs"].lower() == "true":
                 slur = True
             slur_count += 1
